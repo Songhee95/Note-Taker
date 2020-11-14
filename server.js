ee2ac47e4ab0aa93168c051ebac82b9e5537cf94
songhee <thdgml5537@gmail.com>
2020-11-14 15:52:44 -0500

    when focusout from notetext, it will be saved

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const PORT = process.env.PORT || 8080;
 const app = express();
+const path = require('path');
 
 app.use(express.urlencoded({extended: true}));
 app.use(express.json());
+
 
 
 app.listen(PORT, function(){
